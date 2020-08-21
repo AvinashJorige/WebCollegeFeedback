8e45d56ca9f428c1a19c7d97f40f4a4b752d98d2
Avinash Jorige <jo.avi.1990@gmail.com>
2020-08-21 20:23:38 +0530

    Registration Page added for the student with client side validation

--- WebCollegeFeedback/App_Start/RouteConfig.cs
+++ WebCollegeFeedback/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace WebCollegeFeedback
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ WebCollegeFeedback/Controllers/CollegeManagementController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCollegeFeedback.Controllers
+{
+    [RoutePrefix("Colleges")]
+    public class CollegeManagementController : Controller
+    {
+        [Route("AddCollege")]
+        // GET: CollegeManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
