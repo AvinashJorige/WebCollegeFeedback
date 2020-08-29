7f68d6aa465e8bd5f204246bf0efad06b14237ab
Avinash Jorige <jo.avi.1990@gmail.com>
2020-08-29 15:53:08 +0530

    College management is completed. Crud operation of it.

--- WebCollegeFeedback/Controllers/CollegeManagementController.cs
+++ WebCollegeFeedback/Controllers/CollegeManagementController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using ServiceLayer;
 using ServiceLayer.Interfaces;
 using DomainLayer;
+using System.Collections.Generic;
 
 namespace WebCollegeFeedback.Controllers
 {
@@ -49,7 +50,7 @@ namespace WebCollegeFeedback.Controllers
                     }
                     else
                     {
-                        fname = file.FileName;
+                        fname = colleges.CollegeName.Replace(" ", "_") + "_" + DateTime.Now.Ticks + "." + file.FileName.Split('.')[1];
                     }
 
                     colleges.Image = "//Uploads//CollegeImages//" + fname;
@@ -69,6 +70,98 @@ namespace WebCollegeFeedback.Controllers
             }
             // Returns message that successfully uploaded  
             return Json("New College details are added successfully.");
+        }
+
+        [HttpGet]
+        [Route("CollegeList")]
+        public JsonResult GetCollege()
+        {
+            return Json(new { data = collegesService.GetCollegesList() }, JsonRequestBehavior.AllowGet);
+        }
+        
+        [HttpGet]
+        [Route("FindCollege/{collegeId}/College")]
+        public JsonResult GetCollege(string collegeId)
+        {
+            if (string.IsNullOrEmpty(collegeId))
+            {
+                return null;
+            }
+
+            List<CollegesModel> CollegeList = new List<CollegesModel>();
+            CollegeList = (List<CollegesModel>)collegesService.GetCollegesList();
+
+            CollegesModel collegesModel = CollegeList.Find(x => (x.CollgId).Trim().Equals(collegeId.Trim()));
+
+            CollegeList.Clear();
+            CollegeList = null;
+
+            return Json(new { data = collegesModel }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult UpdateCollegeDetails(CollegesModel model)
+        {
+            object obj = new { Code = 500, message = string.Empty };
+            if (model != null)
+            {
+                HttpFileCollectionBase files = Request.Files;
+                //string path = AppDomain.CurrentDomain.BaseDirectory + "Uploads/";  
+                //string filename = Path.GetFileName(Request.Files[i].FileName);  
+
+                HttpPostedFileBase file = files[0];
+                string fname;
+
+                // Checking for Internet Explorer  
+                if (Request.Browser.Browser.ToUpper() == "IE" || Request.Browser.Browser.ToUpper() == "INTERNETEXPLORER")
+                {
+                    string[] testfiles = file.FileName.Split(new char[] { '\\' });
+                    fname = testfiles[testfiles.Length - 1];
+                }
+                else
+                {
+                    fname = model.CollegeName.Replace(" ", "_") + "_" + DateTime.Now.Ticks + "." + file.FileName.Split('.')[1];
+                }
+
+                model.Image = "//Uploads//CollegeImages//" + fname;
+
+                // Get the complete folder path and store the file inside it.  
+                fname = Path.Combine(Server.MapPath("~/Uploads/CollegeImages/"), fname);
+                file.SaveAs(fname);
+
+
+
+                var data = collegesService.UpdateCollegeDetails(model);
+                if(data != null)
+                {
+                    obj = new { Code = 200, message = "Updating the college is successful." };
+                }
+            }
+            else
+            {
+                obj = new { Code = 501, message = "Sorry ! Something went wrong, update is not done." };
+            }
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public ActionResult DeleteCollegeDetails(string CollegeId)
+        {
+            object obj = new { Code = 500, message = string.Empty };
+            if (!string.IsNullOrEmpty(CollegeId))
+            {
+                var data = collegesService.DeleteCollegeDetails(CollegeId);
+                if (data != null)
+                {
+                    obj = new { Code = 200, message = "College details are deleted successfully." };
+                }
+            }
+            else
+            {
+                obj = new { Code = 501, message = "Sorry ! Something went wrong, update is not done." };
+            }
+            return Json(obj, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- WebCollegeFeedback/Scripts/Custom/CollegeManagementScript.js
+++ WebCollegeFeedback/Scripts/Custom/CollegeManagementScript.js
@@ -1,14 +1,19 @@
-﻿$(document).ready(function () {
+﻿var oTblCollegeList = "";
+var EditCollege = "";
+$(document).ready(function () {
+    // on add college button click
     $(".add-clg").click(function () {
         $(".CollegeManagementScreen").fadeOut(500);
         $(".AddCollege").fadeIn(800);
     });
 
+    // on back to college list button click
     $(".back-clg").click(function () {
         $(".AddCollege").fadeOut(500);
         $(".CollegeManagementScreen").fadeIn(800);
+        oTblCollegeList.ajax.reload();
     });
-    
+
     $.validator.addMethod("wordCount", function (value, element, wordCount) {
         return value.split(' ').length <= wordCount;
     });
@@ -30,7 +35,7 @@
             "aboutClg": {
                 required: true,
                 wordCount: 100
-            }, 
+            },
             "clgImg": {
                 required: true,
                 extension: "jpg|jpeg|png|JPG|JPEG|PNG"
@@ -111,7 +116,7 @@
                 });
             } else {
                 alert("FormData is not supported.");
-            } 
+            }
             return false; // for demo
         }
     });
@@ -119,6 +124,102 @@
     $("#btnCollegeReset, .back-clg").click(function () {
         AddCollege.resetForm();
     });
+
+
+    EditCollege = $("#CollegeDetailFormEdit").validate({
+        rules: {
+            "txtCollegeName": {
+                required: true,
+                minlength: 5
+            },
+            "txtClgAddress": {
+                required: true,
+                minlength: 5
+            },
+            "txtClgLink": {
+                required: true,
+                minlength: 10
+            },
+            "txtClgAbout": {
+                required: true,
+                wordCount: 100
+            },
+            "clgImg": {
+                required: true,
+                extension: "jpg|jpeg|png|JPG|JPEG|PNG"
+            }
+        },
+        messages: {
+            "txtCollegeName": {
+                required: "Please, enter College name"
+            },
+            "txtClgAddress": {
+                required: "Please, enter address"
+            },
+            "txtClgLink": {
+                required: "Please, enter college URL link"
+            },
+            "txtClgAbout": {
+                required: "Please, write about the college details",
+                wordCount: "Please, write about the college in maximum 100 words only"
+            },
+            "clgImg": {
+                required: "Please, upload the college photo.",
+                extension: "Please, upload image only with format .JPG / .PNG"
+            }
+        },
+        submitHandler: function (form) { // for demo
+
+            var ClgFileUpload = $("#txtClgImage").get(0);
+            var formObj = [
+                { "key": "CollgId",      "value": $("#txtCollegeId").val() },
+                { "key": "CollegeName",  "value": $("#txtCollegeName").val() },
+                { "key": "Link",         "value": $("#txtClgLink").val() },
+                { "key": "Address",      "value": $("#txtClgAddress").val() },
+                { "key": "AboutCollege", "value": $("#txtClgAbout").val() },
+            ];
+            var URL = '../Colleges/UpdateCollegeDetails';
+
+            OperateCollegeDetails(ClgFileUpload, formObj, URL, function (res) {
+                if (res.Code == 200) {
+                    oTblCollegeList.ajax.reload();
+                    $('#myModal').modal('hide');
+                    swal("Gotcha!", res.message, "success");
+                }
+                else {
+                    oTblCollegeList.ajax.reload();
+                    $('#myModal').modal('hide');
+                    swal("Sorry!", res.message, "error");
+                }
+                
+            });
+            return false;
+        }
+    })
+
+    bindCollegeList();
+
+    $("#btnClgFormEdit").click(function () {
+        try {
+            $(".CollegeDetails").hide();
+            $(".CollegeDetailsEdit").show();
+
+            $("#btnClgFormEdit, #btnCls").hide();
+            $("#btnClgFormSubmit, #btnClgFormBack").show();
+
+        } catch (e) {
+            console.log(e);
+        }
+    })
+
+    $("#btnClgFormBack").click(function () {
+        $(".CollegeDetails").show();
+        $(".CollegeDetailsEdit").hide();
+
+        $("#btnClgFormEdit, #btnCls").show();
+        $("#btnClgFormSubmit, #btnClgFormBack").hide();
+    });
+
 })
 
 
@@ -130,4 +231,175 @@ function ValidateSize(file) {
     } else {
         return true;
     }
+}
+
+function bindCollegeList() {
+    //try {
+    oTblCollegeList = $(".tblColgList").DataTable({
+        'processing': true,
+        'language': {
+            'loadingRecords': '&nbsp;',
+            'processing': 'Loading...'
+        },
+        "ajax": {
+            "url": 'CollegeList',
+            "type": "get",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "CollgId", "autoWidth": true },
+            // { "data": "CollegeName", "autoWidth": true },
+            {
+                "data": "CollegeName", "render": function (data, type, row) {
+
+                    return "<a class='popup' href='javascript:LoadCollegeData(\"" + row.CollgId + "\", \"" + row.CollegeName + "\")'><b>" + row.CollegeName + "</b></a>";
+                }, "width": "180px"
+            },
+            {
+                "data": "Link", "autoWidth": true, "render": function (data, type, row) {
+                    return truncateString(row.Link, 30);
+                }
+            },
+            {
+                "data": "Address", "autoWidth": true, "render": function (data, type, row) {
+                    return truncateString(row.Address, 30);
+                }
+            },
+            {
+                "data": "AboutCollege", "autoWidth": true, "render": function (data, type, row) {
+                    return truncateString(row.AboutCollege, 30);
+                }
+            },
+            {
+                "data": "CollgId", "width": "50px", "className":"dt-body-center", "render": function (data) {
+                    return "<a class='popup text-danger' href='javascript:DeActivateCollege(\"" + data + "\")'><i class='fa fa-2x fa-times'></i></a>";
+                }
+            },
+        ]
+
+    });
+    //} catch (e) {
+    //    console.log(e);
+    //}  href="/Colleges/FindCollege/' + data + '/College"
+}
+
+function LoadCollegeData(CollegeId, CollegeName) {
+
+    $(".CollegeDetailsEdit").hide();
+    $(".CollegeDetails").show();
+
+    $("#btnClgFormEdit, #btnCls").show();
+    $("#btnClgFormSubmit, #btnClgFormBack").hide();
+    
+
+    $(".clgTitle").html(CollegeName);
+
+    Common.Ajax("GET", "FindCollege/" + CollegeId + "/College", null, "JSON", function (res) {        
+        var ClgData = res.data;
+        $("#imgCollegeImage").attr("src", ClgData.Image.replace("//Uploads//CollegeImages//", '/Uploads/CollegeImages/'));        
+        $("#collegeId, #txtCollegeId").val(ClgData.CollgId);
+        $("#collegeName, #txtCollegeName").val(ClgData.CollegeName);
+        $("#collegeLink, #txtClgLink").val(ClgData.Link);
+        $("#collegeAbout, #txtClgAbout").text(ClgData.AboutCollege);
+        $("#CollegeAddress, #txtClgAddress").text(ClgData.Address);
+
+        $('#myModal').modal('show');
+
+        EditCollege.resetForm();
+    }, false, false);
+}
+
+function DeActivateCollege(CollegeId) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, it will be removed from our database forever!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+
+                $.ajax({
+                    url: '../CollegeManagement/DeleteCollegeDetails',
+                    type: "POST",
+                    contentType: "application/json;charset=utf-8", // Not to set any content header  
+                    processData: false, // Not to process data  
+                    data: JSON.stringify({
+                        CollegeId: CollegeId
+                    }),
+                    success: function (res) {
+                        if (res.Code == 200) {
+                            oTblCollegeList.ajax.reload();
+                            swal("Poof! College details are deleted!", {
+                                icon: "success",
+                            });
+                        }
+                        else {
+                            swal("Sorry!", res.message, "error");
+                        }
+                    },
+                    error: function (err) {
+                        alert(err.statusText);
+                    }
+                });
+            } else {
+                swal("Your college details are safe for now!");
+            }
+        });
+}
+
+function OperateCollegeDetails(ClgFileUpload, formObj, URL, ResFunction) {
+    if (window.FormData !== undefined) {
+
+        var fileUpload = ClgFileUpload; // $("#txtImage").get(0);
+        var files = fileUpload.files;
+
+        if (files.length > 1) {
+            alert("Please select one file only");
+            return false;
+        }
+
+        if (!ValidateSize(files)) {
+            alert('File size exceeds 2 MB');
+            return false;
+        }
+
+        // Create FormData object  
+        var fileData = new FormData();
+
+        // Looping over all files and add it to FormData object  
+        for (var i = 0; i < files.length; i++) {
+            fileData.append(files[i].name, files[i]);
+        }
+
+        for (var i in formObj) {
+            fileData.append(formObj[i]["key"], formObj[i]["value"]);
+        }
+
+        $.ajax({
+            url: '../CollegeManagement/UpdateCollegeDetails',
+            type: "POST",
+            contentType: false, // Not to set any content header  
+            processData: false, // Not to process data  
+            data: fileData,
+            success: ResFunction,
+            error: function (err) {
+                alert(err.statusText);
+            }
+        });
+    } else {
+        alert("FormData is not supported.");
+    }
+}
+
+
+function truncateString(str, num) {
+    // If the length of str is less than or equal to num
+    // just return str--don't truncate it.
+    if (str.length <= num) {
+        return str
+    }
+    // Return str truncated with '...' concatenated to the end of str.
+    return str.slice(0, num) + '...'
 }
